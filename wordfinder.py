86d27c2c1eab87cc1303a43d76052434d72155b1
michaelpappas <pappas.michael@gmail.com>
2022-11-29 16:09:03 -0800

    printing text file length

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,15 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, file):
+        """creates a variable file from the argument passed in"""
+        self.file = file
+        self.words = open(f'{self.file}', 'r')
+        self.length = len(self.words.readlines())
+        print(f"{self.length} words read")
+
+    def random(self):
+        pass
+
