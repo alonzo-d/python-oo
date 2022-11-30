d3128acf0517f2b2ec09b67dff0770a71f67815f
David A. Salazar <davidalonzso@gmail.com>
2022-11-29 16:47:34 -0800

    update to init function

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,6 @@
 from random import choice
 
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
@@ -7,9 +8,15 @@ class WordFinder:
         """creates a variable file from the argument passed in"""
         self.file = file
         self.words = open(f'{self.file}', 'r')
-        self.length = len(self.words.readlines())
+        self.words_list = self.words.readlines()
+        self.new_list = [word.strip() for word in self.words_list]
+        self.length = len(self.new_list)
         print(f"{self.length} words read")
 
-    def random(self):
-        pass
+        #new_list = [item.strip() for item in my_list]
 
+    def random(self):
+        return choice(self.new_list)
+
+
+class SpecialWordFinder(WordFinder):
